db72361e8edfcb6265c7c8792c3f655c084c5a16
Katja Levring <katj6233@stud.kea.dk>
2021-11-08 12:55:37 +0100

    page-grundskoler

---
+++ vores-tema/page-grundskoler.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+?>
+
+<template>
+      <article>
+        <img src="" alt="" />
+        <div>
+        <h2></h2>
+        <p class="beskrivelse"></p>
+        <p class="aergang"></p>
+        <p class="fag"></p>
+        </div>
+        </article>
+        </template>
+
+	    <section id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+        <section class="kursuscontainer"></section>
+        </main><!-- #main -->
+
+        <script>
+
+        let kurser;
+		const url = "https://oliviabang.dk/kea/09_CMS/ungdomsbyen/wordpress/wp-json/wp/v2/kursus?per_page=100";
+
+
+		async function getJson() {
+  		const response = await fetch(url);
+  		kurser = await response.json();
+        console.log(kurser);
+  		visKurser();
+		}
+
+        function visKurser() {
+            let temp = document.querySelector("template");
+            let container = document.querySelector(".kursuscontainer");
+            kurser.forEach(kursus =>  {
+            let klon = temp.cloneNode(true).content;
+            klon.querySelector("h2").textContent = kursus.title.rendered;
+            klon.querySelector("img").src = kursus.billede.guid;
+            klon.querySelector(".beskrivelse").textContent = kursus.beskrivelse;
+            klon.querySelector(".fag").textContent = kursus.beskrivelse;
+            klon.querySelector(".aergang").textContent = kursus.pris + " kr.";
+            // klon.querySelector("article").addEventListener("click", () => {location.href = ret.link;})
+            container.appendChild(klon);
+            })
+        }
+
+        getJson ();
+        </script>
+
+		
+	</section><!-- #primary -->
+
+<?php
+
